a0e3179a33340ac9860de02f5a18a5ef934da7bb
JoaquinCardonaRuiz <39555324+JoaquinCardonaRuiz@users.noreply.github.com>
2019-07-14 16:10:17 -0300

    Agregado metodo para cargar texto separado por categorias

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -76,6 +76,101 @@ def leer_archivo():
     return diccionario
 
 
+def leer_archivo_separado():
+    #Vamos a leer pdf con el discurso de los 6 candidatos
+    discurso_6_candidatos = PdfFileReader(open("datos/Version-taquigrafica.pdf", "rb"))
+
+
+    #Capturar la cantidad de paginas que tiene el documento
+    paginas = discurso_6_candidatos.getNumPages()
+    print('Cantidad de páginas:',paginas)
+
+
+    texto_discurso_6 = ""
+    for i in range(paginas):
+        pagina_i = discurso_6_candidatos.getPage(i)
+        texto_i = pagina_i.extractText()
+        texto_discurso_6 = texto_discurso_6 + ' ' + texto_i.replace('\n',"")
+
+    #Los nombres de las categorias estan hardcodeados para reconocerlos en el texto, ya que 
+    #la libreria que se utiliza para leer pdfs no reconoce el subrayado que los marca como titulos
+
+    #Cada elemento de categorias es una seccion, que contiene:
+    #     -el caracter donde comienza (contando el titluo)
+    #     -el caracter donde comienza (sin contarlo)
+    #     -el caracter donde termina
+    #     -el texto que contiene
+    #     -el diccionario que se obtendra como resultado del preprocesamiento
+
+    #Ejemplo de uso: categorias["Desarrollo económico y humano"]["Diccionario"]["Macri"]
+
+    categorias = {"Introducción": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]}, 
+                  "Desarrollo económico y humano": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]},
+                  "Educación e infancia": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]},
+                  "Seguridad y derechos humanos": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]},
+                  "Fortalecimiento democrático": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]},
+                  "Cierre de candidatos": {"Comienzo_titulo":0, "Comienzo_sec":0,"Fin":0,"Texto":[],"Diccionario":[]}}
+
+    #Obtenemos la lista de nombres de categorias
+    keys = list(categorias.keys())
+    for cat in range(len(keys)):
+        #para cada categoria obtenemos comienzos y fin
+        nom_cat = keys[cat]
+        categorias[nom_cat]["Comienzo_titulo"] = texto_discurso_6.find(nom_cat)
+        categorias[nom_cat]["Comienzo_sec"] = categorias[nom_cat]["Comienzo_titulo"] + len(nom_cat)
+        if cat == len(categorias) - 1:
+            categorias[nom_cat]["Fin"] = len(texto_discurso_6) - 1
+        else:
+            categorias[nom_cat]["Fin"] = texto_discurso_6.find(keys[cat+1]) - 1
+
+        #Obtenemos el camo textp, separando el texto segun los comienzos y fines
+        categorias[nom_cat]["Texto"] = texto_discurso_6[categorias[nom_cat]["Comienzo_sec"]:categorias[nom_cat]["Fin"]]
+
+        #Realizamos el preprocesamiento necesario
+        t = categorias[nom_cat]["Texto"]
+        diccionario = dict()
+        lista_discurso = t.split()
+        iteraciones = len(lista_discurso)
+
+        for t in range(iteraciones):
+            #print(t,lista_discurso[t])
+            if lista_discurso[t] == 'Sr.' or lista_discurso[t] == 'Sra.':#Empieza a hablar
+                #print(lista_discurso[t],lista_discurso[t+1])
+                aux = t + 1
+                #print(aux,lista_discurso[aux])
+                if '.-' in lista_discurso[aux]:
+                    candidato = lista_discurso[aux].replace('.-',"")
+                    aux += 1
+                else:
+                    candidato = lista_discurso[aux] + ' ' + lista_discurso[aux+1].replace('.-',"")
+                    aux+=2
+                if not candidato in diccionario:
+                    diccionario[candidato] = ""
+                while lista_discurso[aux] != 'Sr.' and lista_discurso[aux] != 'Sra.' and aux < iteraciones -1:#Empieza a hablar otro
+                    try:
+                        diccionario[candidato] = diccionario[candidato] + " " + lista_discurso[aux]
+                        aux += 1
+                    except:
+                        pass
+                t = aux
+
+        #Quito lo que dicen los conductores
+        for conductor in ['Barili','Bonelli','Novaresio']:
+            if conductor in diccionario:
+                del diccionario[conductor]
+
+        #Descargar diccionario en csv separado por ;
+        with open('dicurso_por_candidato.csv', 'w') as f:
+            for key in diccionario.keys():
+                f.write("%s;%s\n"%(key,diccionario[key]))
+
+        categorias[nom_cat]["Diccionario"] =  diccionario
+
+    return categorias
+
+
+
+
 #Normalización
 #poner todo el texto en igualdad de condiciones: convirtiendo todo el texto en el
 #mismo caso (superior o inferior), eliminando la puntuación, convirtiendo los números
